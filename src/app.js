240720df40bbeb121d80a8a8fc257342b45b16c8
Theadd <pantallazo@gmail.com>
2015-09-17 06:09:34 +0200

    ribbon

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import SplitPane from 'react-split-pane'
 import SourceView from './views/sourceview/sourceview'
 import ComponentView from './views/componentview/componentview'
 import svg2react from './lib/svg2react'
+import GitHubForkRibbon from 'react-github-fork-ribbon'
 
 import './app.css'
 
@@ -27,6 +28,9 @@ export class App extends Component {
 
     return (
       <main>
+        <GitHubForkRibbon href="https://github.com/Theadd/svg2react" target="_blank" position="left-bottom">
+          Fork me on GitHub
+        </GitHubForkRibbon>
         <SplitPane split="vertical" minSize="150" defaultSize="50%">
           <div style={{ width: '100%', height: '100%', position: 'absolute' }}>
             <SourceView onChange={ this.handleSourceChange } liveUpdate={ true } />
